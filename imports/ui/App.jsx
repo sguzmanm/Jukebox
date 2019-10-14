ce488f33dbc7e011791ff8c2885e776a3297006b
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-10-14 13:32:50 -0500

    Update App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,7 @@ import NotFound from "./NotFound.jsx";
 
 export const SCOPE =
   "user-read-private user-read-email user-read-currently-playing user-read-playback-state playlist-modify-public playlist-modify-private playlist-read-private user-top-read";
+// sguzmanm: I am not a 100% sure that your client credentials should be burnt in the client code. It´s better to have env variables ofor this things
 export const CLIENT_ID = "11b6f67236e247b2a05d70cf8b819b51";
 export const CLIENT_SECRET = "016e987e71cb4cc9a0d630d7b56449f5";
 export const REDIRECT_URI = "http://localhost:3000/create";
