fecddc4de81b296324d03a5607f2a0d168b6d4e5
Nicolas Caceres <nicolascaceresacosta@hotmail.com>
2019-10-13 23:43:58 -0500

    Listo el eslint

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 import { Meteor } from "meteor/meteor";
+/*global describe*/
 describe("Jukebox", function() {
   it("package.json has correct name", async function() {
     const { name } = await import("../package.json");
@@ -13,6 +14,7 @@ describe("Jukebox", function() {
   }
 
   if (Meteor.isServer) {
+    /*global it*/
     it("server is not client", function() {
       assert.strictEqual(Meteor.isClient, false);
     });
